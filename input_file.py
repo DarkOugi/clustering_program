b9c5c6d78aa1f9cdcc463afba325a512935ce1d5
Ougi <night.ougi@gmail.com>
2023-06-27 03:39:56 +0500

    second commit

--- input_file.py
+++ input_file.py
@@ -1,10 +1,62 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+import spacy
 import sklearn
 from sklearn.cluster import KMeans
 
-if __name__ == '__main__':
+STOPWORD = stopwords.words(['russian', 'english'])
+
+
+def preprocessing_text(path):
     data = []
-    path_file = input('Введите путь к файлу и имя файла: ')
-    with open(path_file,'r',encoding='utf-8') as f:
+    with open(path, 'r', encoding='utf-8') as f:
         for string in f.readlines():
-            data.append(string.split())
+            text = string.lower()
+            clean_text = re.sub(r'\W+', ' ', text)
+            data.append([s for s in clean_text.split() if s not in STOPWORD])
     print(data)
+    print(stemming(data))
+    print(lemming(data))
+
+
+def lemming(data):
+    '''
+    Подумать над этим алгоритмом
+    '''
+    pass
+
+
+def stemming(data):
+    snowball_rus = SnowballStemmer(language="russian")
+    snowball_eng = SnowballStemmer(language="english")
+    stemming_data = []
+    for string in data:
+        stemming_str = []
+        for word in string:
+            if 97 <= ord(word[0]) <= 122:
+                stemming_str.append(snowball_eng.stem(word))
+            else:
+                stemming_str.append(snowball_rus.stem(word))
+        stemming_data.append(stemming_str)
+    return stemming_data
+
+
+def tf_idf(data):
+    size = len(data)
+    worlds = {}
+    for string in data:
+        for word in string:
+            if word in worlds:
+                worlds[word] += 1
+            else:
+                worlds[word] = 1
+    for word in worlds:
+        worlds[word] /= size
+    print(worlds)
+
+
+if __name__ == '__main__':
+    path_file = input('Введите путь к файлу и имя файла: ')
+    preprocessing_text(path_file)
